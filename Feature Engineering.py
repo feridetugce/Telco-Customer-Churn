7ea2acbb9fd38cf0f36aef1ce0688e6d2d0a848f
Feride Tuğçe Deveci Bayraktar <ftugce.deveci@gmail.com>
2023-01-22 23:32:05 +0300

    Update Feature Engineering.py

--- Feature Engineering.py
+++ Feature Engineering.py
@@ -1,5 +1,5 @@
 df = pd.read_csv("Telco-Customer-Churn.csv")
-df = df.copy()
+df_Telco = df.copy()
 
 def check_df(dataframe, head=5):
     print("############### Shape ################")
@@ -17,4 +17,33 @@ def check_df(dataframe, head=5):
     print ( "########### Nunique ###############" )
     print ( dataframe.nunique())
 
-check_df(df)
+check_df(df_Telco)
+
+df_Telco["TotalCharges"] = pd.to_numeric(df_Telco["TotalCharges"], errors='coerce')
+df_Telco["Churn"] = df_Telco["Churn"].apply(lambda x : 1 if x == "Yes" else 0)
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+    
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df_Telco)
+
+
